87cd5e24f83cb0befa2ac0db7dda38c63fa9fb11
Ammar Lakho <ammar1lakho@gmail.com>
2025-07-01 19:34:56 +0200

    v3

--- app/src/main/kotlin/competition_entry/RunEntryAsServer.kt
+++ app/src/main/kotlin/competition_entry/RunEntryAsServer.kt
@@ -1,8 +1,9 @@
 package competition_entry
 
+import games.planetwars.agents.strategic.TeamTitansAgentV3
 import json_rmi.GameAgentServer
 
 fun main() {
-    val server = GameAgentServer(port = 8080, agentClass = GreedyHeuristicAgent::class)
+    val server = GameAgentServer(port = 8080, agentClass = TeamTitansAgentV3::class)
     server.start(wait = true)
 }
